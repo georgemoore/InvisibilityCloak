0227437be9506a3897e50432b25cd47f6c5ffbc1
George Moore <g.moore@ulster.ac.uk>
2019-08-27 17:41:46 +0100

    Functions added
    Function to process keyboard aded, along with trays for mask HSV limits
    Function added for setting backgorund

--- Invisibility_Cloak.py
+++ Invisibility_Cloak.py
@@ -2,43 +2,90 @@ import cv2
 import numpy as np
 import time
 
-print("Invisibility Cloak")
+
+def set_background(cam):
+    ret, background = cam.read()
+    background = np.flip(background, axis=1)
+    print("Background Set")
+    return background
+
+
+def process_keyboard(k):
+
+    # Adjust Mask HSV upper limits
+    if k == 49:
+        mask_hsv_lower_limit[0] += 10
+        print("low_h", mask_hsv_lower_limit[0])
+    if k == 50:
+        mask_hsv_lower_limit[1] += 10
+        print("low_s", mask_hsv_lower_limit[1])
+    if k == 51:
+        mask_hsv_lower_limit[2 ] += 10
+        print("low_v", mask_hsv_lower_limit[2])
+    if k == 113:
+        mask_hsv_lower_limit[0] -= 10
+        print("low_h", mask_hsv_lower_limit[0])
+    if k == 119:
+        mask_hsv_lower_limit[1] -= 10
+        print("low_s", mask_hsv_lower_limit[1])
+    if k == 101:
+        mask_hsv_lower_limit[2] -= 10
+        print("low_v", mask_hsv_lower_limit[2])
+
+    # Adjust Mask HSV lower limits
+    if k == 97:
+        mask_hsv_upper_limit[0] += 10
+        print("high_h", mask_hsv_upper_limit[0])
+    if k == 115:
+        mask_hsv_upper_limit[1] += 10
+        print("high_s", mask_hsv_upper_limit[1])
+    if k == 100:
+        mask_hsv_upper_limit[2] += 10
+        print("high_v", mask_hsv_upper_limit[2])
+    if k == 122:
+        mask_hsv_upper_limit[0] -= 10
+        print("high_h", mask_hsv_upper_limit[0])
+    if k == 120:
+        mask_hsv_upper_limit[1] -= 10
+        print("high_s", mask_hsv_upper_limit[1])
+    if k == 99:
+        mask_hsv_upper_limit[2] -= 10
+        print("high_v", mask_hsv_upper_limit[2])
+
+    # Update the background image
+    if k == 32:
+        background = set_background(cam)
+
+
 
 # Initialise mask levels (Green)
-low_h = 30
-low_s = 0
-low_v = 70
-high_h = 80
-high_s = 255
-high_v =255
+mask_hsv_lower_limit = [30, 0, 70]
+mask_hsv_upper_limit = [80, 255, 255]
+
+print("Invisibility Cloak")
 
 # Capture static Background image
 # cam = cv2.VideoCapture(0)   # Internal camera
 cam = cv2.VideoCapture(1)   # External Camera
-ret, background = cam.read()
-background = np.flip(background, axis=1)
+background = set_background(cam)
 
 #Apply mask to live image
 while (cam.isOpened()):
 
+    #Get live camera image
     ret, img = cam.read()
     img = np.flip(img, axis=1)
 
+    #Create Mask
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # value = (35, 35)
-    # blurred = cv2.GaussianBlur(hsv, value,0)
-
-    #Create Mask - Green
-    lower_colour = np.array([low_h, low_s, low_v])
-    upper_colour = np.array([high_h, high_s, high_v])
-    mask = cv2.inRange(hsv, lower_colour, upper_colour)
-
+    mask_lower_limit = np.array([mask_hsv_lower_limit[0], mask_hsv_lower_limit[1], mask_hsv_lower_limit[2]])
+    mask_upper_limit = np.array([mask_hsv_upper_limit[0], mask_hsv_upper_limit[1], mask_hsv_upper_limit[2]])
+    mask = cv2.inRange(hsv, mask_lower_limit, mask_upper_limit)
 
     # Remove small mask glithces
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
-    #Modify image where masked
+    #Replace live image with background where masked
     img[np.where(mask == 255)] = background[np.where(mask == 255)]
 
     #Display modified image
@@ -47,51 +94,8 @@ while (cam.isOpened()):
     # Reset waitKey delay, which doubles as output frame rate control in m seconds
     k = cv2.waitKey(1)
 
-    # Adjust Mask HSV upper limits
-    if k == 49:
-        low_h += 10
-        print("low_h", low_h)
-    if k == 50:
-        low_s += 10
-        print("low_s", low_s)
-    if k == 51:
-        low_v += 10
-        print("low_v", low_v)
-    if k == 113:
-        low_h -= 10
-        print("low_h", low_h)
-    if k == 119:
-        low_s -= 10
-        print("low_s", low_s)
-    if k == 101:
-        low_v -= 10
-        print("low_v", low_v)
-
-    # Adjust Mask HSV lower limits
-    if k == 97:
-        high_h += 10
-        print("high_h", high_h)
-    if k == 115:
-        high_s += 10
-        print("high_s", high_s)
-    if k == 100:
-        high_v += 10
-        print("high_v", high_v)
-    if k == 122:
-        high_h -= 10
-        print("high_h", high_h)
-    if k == 120:
-        high_s -= 10
-        print("high_s", high_s)
-    if k == 99:
-        high_v -= 10
-        print("high_v", high_v)
-
-    # Update the background image
-    if k == 32:
-        ret, background = cam.read()
-        background = np.flip(background, axis=1)
-        print("Background Updated")
+    if k > -1:
+        process_keyboard(k)
 
     # End While if Esc key pressed
     if k == 27:
